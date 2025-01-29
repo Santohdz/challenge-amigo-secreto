69a6cf45b552f20cbcc4aacfdef8638e78260fdc
Santiago Hernández <santocho04@hotmail.com>
2025-01-29 13:19:47 -0600

    Se añadió salto de línea
    Se mejoró la robustez del codigo, solo permitiendo nombres válidos (sin números) y se implementó un salto de línea entre cada nombre

--- app.js
+++ app.js
@@ -4,27 +4,31 @@ let listaAmigos = [];
 
 function agregarAmigo () {
     let amigo = document.getElementById('amigo').value.trim(); // Obtener el valor del input y eliminar espacios innecesarios al inicio y final
-    if (amigo) {
-        let nombreConFormato = aplicarFormatoNombre(amigo);
+    // Validar que solo contenga letras (sin números ni caracteres especiales)
+    let regex = /^[a-zA-Z\s]+$/; // Solo permite letras y espacios
+    
+    if (amigo && regex.test(amigo)) {
+        let nombreConFormato = aplicarFormatoNombre(amigo); // Aplicar formato
         listaAmigos.push(nombreConFormato); // Agregar el nombre con formato a la lista
         limpiarCaja(); 
         mostrarLista();
     }
     else {
         alert("Por favor, ingresa un nombre válido");
+        limpiarCaja(); 
     }
     console.log (listaAmigos);
 }
 
 function mostrarLista () {
-    document.getElementById('listaAmigos').textContent = listaAmigos.join(", ");
+    document.getElementById('listaAmigos').innerHTML = listaAmigos.join("<br>");
 }
 
 // Limpiar input
 function limpiarCaja () {
     document.querySelector('#amigo').value = "";
 }
-
+ 
 // Convertir la primera letra a mayúscula y el resto a minúscula
 function aplicarFormatoNombre (nombre) {
     return nombre.charAt(0).toUpperCase() + nombre.slice(1).toLowerCase();
