ab85ed3b11885573ed41fbf0db98b3722247494b
Santiago Hernández <santocho04@hotmail.com>
2025-01-29 01:54:15 -0600

    Se añadió el array de los nombres
    Se añadió el array encargado de almacenar los nombres así como la función para añadir cada nombre con un formato adecuado

--- app.js
+++ app.js
@@ -1,1 +1,31 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+'use strict'; //Activando el modo estricto
+
+let listaAmigos = [];
+
+function agregarAmigo () {
+    let amigo = document.getElementById('amigo').value.trim(); // Obtener el valor del input y eliminar espacios innecesarios al inicio y final
+    if (amigo) {
+        let nombreConFormato = aplicarFormatoNombre(amigo);
+        listaAmigos.push(nombreConFormato); // Agregar el nombre con formato a la lista
+        limpiarCaja(); 
+        mostrarLista();
+    }
+    else {
+        alert("Por favor, ingresa un nombre válido");
+    }
+    console.log (listaAmigos);
+}
+
+function mostrarLista () {
+    document.getElementById('listaAmigos').textContent = listaAmigos.join(", ");
+}
+
+// Limpiar input
+function limpiarCaja () {
+    document.querySelector('#amigo').value = "";
+}
+
+// Convertir la primera letra a mayúscula y el resto a minúscula
+function aplicarFormatoNombre (nombre) {
+    return nombre.charAt(0).toUpperCase() + nombre.slice(1).toLowerCase();
+}
